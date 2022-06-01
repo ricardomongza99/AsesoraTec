bf65ebfe1a6bc023ba1af7796c02927ebec27126
Ricardo Montemayor <ricardomongza@gmail.com>
2022-06-01 12:51:24 -0500

    Initial Commit

---
+++ AsesoraTec/AsesoraTecApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsesoraTecApp.swift
+//  AsesoraTec
+//
+//  Created by Ricardo Montemayor on 01/06/22.
+//
+
+import SwiftUI
+
+@main
+struct AsesoraTecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
