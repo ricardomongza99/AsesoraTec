320ccf047ba41026cff9db205cdd3c714a0fb936
Ricardo Montemayor <ricardomongza@gmail.com>
2022-06-01 14:41:04 -0500

    Add `major` as a Tutor property

--- AsesoraTec/Models/Tutor.swift
+++ AsesoraTec/Models/Tutor.swift
@@ -12,6 +12,7 @@ struct Tutor: Identifiable {
     var name: String
     var phone: String
     var courses: [String]
+    var major: String
     var availability: String
     var price: Double
     
@@ -30,10 +31,10 @@ struct Tutor: Identifiable {
 class TutorAPI {
     static func getTutors() -> [Tutor] {
         return [
-            Tutor(name: "Miguel Sánchez", phone: "8111223342", courses: ["Mate 1", "Mate 2", "Mate 3", "Ecuaciones diferenciales"], availability: "Lunes a jueves de 4 a 6", price: 0.0),
-            Tutor(name: "Luis Mario Benitez", phone: "5183421244", courses: ["Electricidad y magnetismo", "Mate 2", "Mate 3", "Física 1", "Física 2"], availability: "Jueves de 4 a 8", price: 100.0),
-            Tutor(name: "Patricio Valerio", phone: "833218678", courses: ["Análisis y expresión verbal", "Inglés 1"], availability: "Sábados a las 10am", price: 300.0),
-            Tutor(name: "Juan Luis Guerrero", phone: "8112758091", courses: ["Mate 1"], availability: "Lu/Mi 8pm a 10pm", price: 200.0),
+            Tutor(name: "Miguel Sánchez", phone: "8111223342", courses: ["Mate 1", "Mate 2", "Mate 3", "Ecuaciones diferenciales"], major: "IQA", availability: "Lunes a jueves de 4 a 6", price: 0.0),
+            Tutor(name: "Luis Mario Benitez", phone: "5183421244", courses: ["Electricidad y magnetismo", "Mate 2", "Mate 3", "Física 1", "Física 2"], major: "IME", availability: "Jueves de 4 a 8", price: 100.0),
+            Tutor(name: "Patricio Valerio", phone: "833218678", courses: ["Análisis y expresión verbal", "Inglés 1"], major: "LED", availability: "Sábados a las 10am", price: 300.0),
+            Tutor(name: "Juan Luis Guerrero", phone: "8112758091", courses: ["Mate 1"], major: "INT", availability: "Lu/Mi 8pm a 10pm", price: 200.0),
         ]
     }
 }
